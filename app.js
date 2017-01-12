904b7abc71968e446853a5816b23ba4967192ae9
Michal Bodzianowski <Michal Bodzianowski>
2017-01-11 23:35:13 -0700

    lols

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var bp = require('body-parser');
 
-var madness = 10;
+
+var madness = start_mad;
+var end_mad = 20;
+var start_mad = Math.round(end_mad/2);
+var used = [];
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bp.json());
@@ -18,22 +22,30 @@ app.post('/webhook', function(rq, rs){
   var r = rq.body.result.fulfillment.speech;
   console.log(b);
   if(!isNaN(b)){
-    madness += parseInt(b);
+    if(used.indexOf(rq.body.metadata.intentName) == -1){
+      madness += parseInt(b);
+    }
   }
 
 
   rs.set('Content-Type', 'application/json');
-  if(rq.body.result.parameters.reset == "r"){
+  var rst = rq.body.result.parameters.reset;
+  if(rst == "r"){
     madness = 10;
+    used = [];
+  }else if(!isNaN(rst)){
+    end_mad = rst;
   }
 
   if(madness <= 0){
     rs.send({"speech": "im mad so ww1 happened", "displayText":"im mad so ww1 happened"});
 
+  }else if (madness >= end_mad){
+    rs.send({"speech": "im happy so ww1 didn't happen", "displayText":"im happy so ww1 didn't happen"});
   }else{
-    rs.send({"speech": (r+" (Temperment: " + madness + ")"), "displayText":(r+" (Temperment: " + madness+")")});
+    rs.send({"speech": (r+" (Temperament: " + madness + ")"), "displayText":(r+" (Temperament: " + madness+")")});
   }
-
+  used.push(rq.body.metadata.intentName);
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
