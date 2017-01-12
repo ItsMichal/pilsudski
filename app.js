ee25d546d76d6d000fc9909af729dc5f24c93bbb
Michal Bodzianowski <Michal Bodzianowski>
2017-01-11 23:04:25 -0700

    lols

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.post('/webhook', function(rq, rs){
   var b = rq.body.result.parameters.trigger;
   var r = rq.body.result.fulfillment.speech;
   console.log(b);
-  if(typeof parseInt(b) === "number")
-    madness += parseInt(b);
+  madness += parseInt(b);
 
   rs.set('Content-Type', 'application/json');
   if(rq.body.result.parameters.reset == "r"){
