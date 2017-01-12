74247af8e4fa5481cf48cc2f1ca3bd4cc056aa75
Michal Bodzianowski <Michal Bodzianowski>
2017-01-11 22:12:16 -0700

    ah

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 
 var madness = 10;
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(rq, rs){
   rs.send("test");
 });
