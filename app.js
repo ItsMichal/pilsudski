199befcfac4d852323f78dda7cac738a0b8a6e9f
Michal Bodzianowski <Michal Bodzianowski>
2017-01-11 20:54:57 -0700

    init 2

---
+++ app.js
@@ -0,0 +1,13 @@
+const apiaibotkit = require('api-ai-botkit');
+const apiai = apiaibotkit(3338480a082e4f9fb41267b47f303f66);
+
+var madness = 10;
+
+apiai
+    .action('smalltalk.greetings', function (message, resp, bot) {
+        var responseText = resp.result.fulfillment.speech;
+        bot.reply(message, responseText);
+    })
+    .action('input.unknown', function (message, resp, bot) {
+        bot.reply(message, "Sorry, I don't understand");
+    });
