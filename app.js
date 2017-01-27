04d3464043e44049bb592c7f662d086ccd2602b0
ItsMichal <Michal@Michals-MacBook-Air.local>
2017-01-27 14:35:58 -0700

    undo because I failed
    Revert "merged prototype"
    
    This reverts commit 235454779e8d8948ab245a1e717c4705baffcb59.

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 "use strict";
+
 var express = require('express');
 var app = express();
 var bp = require('body-parser');
 var fs = require('fs');
-var path = require('path');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var https = require('https');
-var urlParse  = require('url').parse;
-var googleTTS = require('google-tts-api');
-var lame = require('lame');
-var wav = require('wav');
+
 /**
 * Author: Michal Bodzianowski C 2017
 * MIT license
@@ -26,7 +20,7 @@ var wav = require('wav');
 
 //Get Data from Config
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-
+console.log(config);
 //Target temperament for victory. The higher the Temperament, the better. Set by default to 20 (The default lose temperament is zero and cannot be changed)
 var winTemper = config.setup.winTemperament;
 
@@ -48,12 +42,9 @@ app.use(bp.json());
 //Probably unneccessary but convention (and ctrl+c / ctrl+v from debugging heroku.com)
 app.use(express.static(__dirname + '/public'));
 
-mu.root = __dirname;
-m
-
 //You can delete this, its pretty unneccessary.
 app.get('/', function(rq, rs){
-  rs.send('app.html');
+  rs.send("test");
 });
 
 //I'll seperate the logic into a seperate function later, but right now that's unneccessary
@@ -62,8 +53,6 @@ app.get('/', function(rq, rs){
 app.post('/webhook', function(rq, rs){
   //Important, API.AI requires this bit
   rs.set('Content-Type', 'application/json');
-  //response
-  var response = "";
 
   //the change in temper value, derived from the JSON given to me from API.AI. the students will change/set this value in API.AI (video tutorial coming soon)
   //might be null/NaN
@@ -86,9 +75,6 @@ app.post('/webhook', function(rq, rs){
     if(staleTexts.indexOf(intent) == -1 /*|| temperDelta < 0*/){
       //finally just add the value to the curTemper, updating it.
       curTemper += parseInt(temperDelta);
-    }else{
-      //do what you said here
-      rs.send(config.responses.stale + " " + aiResponse)
     }
   }
 
@@ -111,30 +97,42 @@ app.post('/webhook', function(rq, rs){
   //If you lose...
   if(curTemper <= 0){
 
-    rs.send({"speech": loseResponse, "displayText":loseResponse});
+    rs.send({"speech": aiResponse+" "+feedback+" "+loseResponse, "displayText":aiResponse+" "+feedback+" "+loseResponse});
   //If you lose...
   }else if (curTemper >= winTemper){
     //or If you win
-    rs.send({"speech": winResponse, "displayText":winResponse});
+    rs.send({"speech": aiResponse+" "+feedback+" "+winResponse, "displayText":aiResponse+" "+feedback+" "+winResponse});
   }else{
     //otherwise Display Response and find the appropriate response based on anger and config.json
-    console.log(response + " & " + JSON.stringify(config.responses) + "&" + config.responses.neutral + "&" +config.responses.happy);
 
-    if(curTemper > (winTemper-(curTemper/4))){
-      repsonse = config.responses.veryhappy;
-    }else if(curTemper > (winTemper-curTemper/2)){
-      repsonse = config.responses.happy;
-    }else if(curTemper < curTemper/2){
-      response = config.responses.angry;
-    }else if(curTemper < ((3*curTemper)/4)){
+    //debugging this took forever, thanks to JS's dynamic data types.
+
+    //IT TOOK FOREVER BECAUSE OF A
+    //FRIGGIN
+    //TYPO
+
+    //rip
+
+    var response = config.responses.neutral;
+    if(curTemper > ((3*winTemper)/4)){
+      response = config.responses.veryhappy;
+    }else if(curTemper > winTemper/2){
+      response = config.responses.happy;
+    }else if(curTemper < winTemper-((3*winTemper)/4)){
       response = config.responses.veryangry;
-    }else{
-      response = config.responses.neutral;
+    }else if(curTemper < winTemper/2){
+      response = config.responses.angry;
     }
+
+    //tells user if they are doing good or nah\
+    //terrible logic, idgaf tho
+    var feedback = config.responses.same;
+    if(!isNaN(temperDelta) && parseInt(temperDelta) != 0){
+      feedback = (parseInt(temperDelta) > 0) ? config.responses.good : config.responses.bad;
+    }
+
     //And then just combine the two
-    rs.send({"speech": (aiResponse+" "+response), "displayText":(aiResponse+" "+response)});
-    tts(aiResponse+" "+response);
-    io.emit('audio-update');
+    rs.send({"speech": (aiResponse+" "+feedback+" "+response), "displayText":(aiResponse+" "+feedback+" "+response)});
   }
 
   //Finally, add the current dialogue to the staleTexts. If it's not already there of course
@@ -146,137 +144,6 @@ app.post('/webhook', function(rq, rs){
   ///*uncomment to debug*/ console.log(rq);
   ///*uncomment to debug*/ console.log(temperDelta);
 });
-
-
-//COOL STFUU
-
-//TTS block
-//from google-tts
-function downloadFile (url, dest) {
-  return new Promise(function (resolve, reject) {
-    var info = urlParse(url);
-    var httpClient = info.protocol === 'https:' ? https : http;
-    var options = {
-      host: info.host,
-      path: info.path,
-      headers: {
-
-        'Accept-Language': 'en-US',
-        'Authorization': 'Bearer c1f828342331434c97ca20be01c3e317'
-
-      }
-    };
-
-    httpClient.get(options, function(res) {
-      // check status code
-      if (res.statusCode !== 200) {
-        reject(new Error('request to ' + url + ' failed, status code = ' + res.statusCode + ' (' + res.statusMessage + ')'));
-        return;
-      }
-
-      var file = fs.createWriteStream(dest);
-      file.on('finish', function() {
-        // close() is async, call resolve after close completes.
-        ptchshift();
-        file.close(resolve);
-
-      });
-      file.on('error', function (err) {
-        // Delete the file async. (But we don't check the result)
-        fs.unlink(dest);
-        reject(err);
-      });
-
-      res.pipe(file);
-    })
-    .on('error', function(err) {
-      reject(err);
-    })
-    .end();
-  });
-}
-
-
-//tts("In spite of the fact that we have no such fleet as we should have, we have conquered for ourselves a place in the sun. It will now be my task to see to it that this place in the sun shall remain our undisputed possession, in order that the sun's rays may fall fruitfully upon our activity and trade in foreign parts, that our industry and agriculture may develop within the state and our sailing sports upon the water, for our future lies upon the water.")
-
-function tts(x){
-  googleTTS(x, 'de', 1.9)
-  .then(function (url) {
-    console.log(url); // https://translate.google.com/translate_tts?...
-    var nurl = "https://api.api.ai/v1/tts?text="+x+"";
-    console.log(nurl);
-    var dest = path.resolve(__dirname, 'base.wav'); // file destination
-    console.log('Download to ' + dest + ' ...');
-
-    return downloadFile(nurl, dest);
-  })
-  .then(function () {
-    console.log('Download success');
-
-  })
-  .catch(function (err) {
-    console.error(err.stack);
-  });
-
-
-}
-
-//sample rate
-var sr = 24000  ;
-// var encoder = new lame.Encoder({
-//   // input
-//   channels: 2,        // 2 channels (left and right)
-//   bitDepth: 16,       // 16-bit samples
-//   sampleRate: 44100,  // 44,100 Hz sample rate
-//
-//   // output
-//   bitRate: 128,
-//   outSampleRate: sr/2,
-//   mode: lame.STEREO // STEREO (default), JOINTSTEREO, DUALCHANNEL or MONO
-// });
-var decoder = new lame.Decoder();
-var inputsound;
-var outputsound;
-function onFormat (format) {
-  console.error('WAV format: %j', format);
-  var mformat = format;
-  mformat.sampleRate = 17000;
-  // encoding the wave file into an MP3 is as simple as calling pipe()
-  var encoder = new lame.Encoder(mformat);
-  decoder.pipe(encoder).pipe(outputsound);
-  console.log("done");
-}
-
-function ptchshift(){
-  inputsound = fs.createReadStream("base.wav");
-  outputsound = fs.createWriteStream("final.mp3");
-  decoder = new wav.Reader();
-  decoder.on('format', onFormat);
-  inputsound.pipe(decoder);
-
-  //old broken stuff
-  // var ac = new AudioContext();
-  // ac.outStream = fs.createWriteStream('final.mp3');
-  // var vaw = ac.createBufferSource();
-  // fs.readFile(__dirname + 'base.mp3', function(err, buffer) {
-  //   if (err) throw err
-  //   ac.decodeAudioData(buffer, function(audioBuffer) {
-  //     vaw = ac.createBufferSource()
-  //     vaw.connect(ac.destination)
-  //     vaw.buffer = audioBuffer
-  //   });
-  // });
-  // var ps = PitchShift(ac);
-  // ps.connect(ac.destination);
-  // ps.transpose = -12;
-  // vaw.connect(ps);
-  // vaw.start();
-  // vaw.stop(ac.currenttime + x);
-  // ac.outStream.end();
-}
-
-
-
 
 //Have the app actually run on the server
 app.listen(app.get('port'), function() {
