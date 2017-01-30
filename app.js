0f62fdf9f4bb6dffb51cc6d77ea06ecaa21b48d6
ItsMichal <michaljbodzianowski@gmail.com>
2017-01-30 15:36:15 -0700

    debug 9

--- app.js
+++ app.js
@@ -213,7 +213,7 @@ function tts(x){
   .then(function (url) {
     console.log(url); // https://translate.google.com/translate_tts?...
     //var nurl = "https://api.api.ai/v1/tts?text="+encodeURIComponent(x)+"";
-    var nurl = 'https://infinite-lake-18103.herokuapp.com/?text="'+encodeURIComponent(x)+'"';
+    var nurl = 'https://infinite-lake-18103.herokuapp.com/?text="'+encodeURIComponent(x)+'"&locale=de';
     console.log(nurl);
     var dest = path.resolve(__dirname, 'base.wav'); // file destination
     console.log('Download to ' + dest + ' ...');
@@ -250,7 +250,7 @@ var outputsound;
 function onFormat (format) {
   console.error('WAV format: %j', format);
   var mformat = format;
-  mformat.sampleRate = format.sampleRate / 1.1;
+  mformat.sampleRate = Math.floor(format.sampleRate / 1.5);
   // encoding the wave file into an MP3 is as simple as calling pipe()
   var encoder = new lame.Encoder(mformat);
   decoder.pipe(encoder).pipe(outputsound);
